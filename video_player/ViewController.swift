f16f38e0e3c3d1c800b9e51dd69e1e9cb4f78d42
SanjulaD <desanjula99@gmail.com>
2023-02-01 23:00:21 +0530

    imports AVKits and AVFoundation modules

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,6 +16,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    private func findVideo() {
+        guard let path = Bundle.main.path(forResource: "sample-5s", ofType: "m4v") else {
+            debugPrint("sample.m4v not found")
+            
+            return
+        }
+    }
 
 }
 
